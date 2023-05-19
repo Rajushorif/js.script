b10efbdce53671756f4f8b7216a07a7ead42511e
RajuShorif <rajushorif48@gmail.com>
2023-05-19 21:28:55 +0600

    git add script.js

--- js.script/script.js
+++ js.script/script.js
@@ -1,43 +1,22 @@
+//Object Method
 const raju = {
-    firstName: "Raju",
-    lastName: 'Shorif',
-    age: 2040 - 2004,
-    job: "Front-end developer",
-    friends:[
-        'Mijan','Ibrahim', 'Nishat'
-    ]
-     }
-     console.log(raju);
-     console.log(raju.lastName);
-     console.log(raju['lastName']);
-     const nameKey = "Name";
-     console.log(raju['first' + nameKey]);
-     console.log(raju['last' + nameKey]);
-    
-     //Pop up 
-     const interestedIn = prompt('what do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
-    //retriving an element from an object
-      if (raju[interestedIn]) {
-        console.log(raju[interestedIn]);    
-      } else{
-        console.log('Wrong request! Choose between firstName, lastName, age, job, and friends')
-      };
-      raju.location = 'Madaripur';
-      raju['twitter'] = '@rajushorif';
-      console.log('raju');
-      console.log(`${raju.firstName} has ${raju.friends.length} friends, and his best friend is called ${raju.friends[2]}`)
-      
-      // Object Methods
-      const raju = {
-        firstName: "Raju",
-        lastName: 'Shorif',
-        age: 2040 - 2004,
-        job: "Front-end developer",
-        friends:[
-            'Mijan','Ibrahim', 'Nishat'
-        ],
-        calcAge : function (birthYear){
-          return 2040 - 2004;
-        }
-         };
-         console.log(raju.calcAge)
+  firstName: "Raju",
+  lastName: "Shorif",
+  job: 'Teacher',//string value
+  birthYear: 1991,
+  friend:['Friend', 'Peter', 'Steven'],//array value
+hasDriversLicense: true,// boolen value
+calcAge: function () {
+  this.age = 2037 - this.birthYear;
+  return this.age;
+},//function value(property of raju expression)
+
+//challenge
+//"Raju is a 23-year old teacher, and he has a driver's licnse"
+getSummary: function() {
+
+ return  `${this.firstName} is a ${this.calcAge()} -year old ${this.birthYear.job}, and he has ${this.hasDriversLicense ? 'a' : 'not'} driver's license.`
+}
+};
+console.log( raju.calcAge(1991)); 
+console.log(raju.getSummary()); 

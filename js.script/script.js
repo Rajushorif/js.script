1e7546572bd578d7b4b86abfa196a624d489cebd
RajuShorif <rajushorif48@gmail.com>
2023-05-20 12:32:31 +0600

    git add

--- js.script/script.js
+++ js.script/script.js
@@ -1,22 +1,35 @@
-//Object Method
-const raju = {
-  firstName: "Raju",
-  lastName: "Shorif",
-  job: 'Teacher',//string value
-  birthYear: 1991,
-  friend:['Friend', 'Peter', 'Steven'],//array value
-hasDriversLicense: true,// boolen value
-calcAge: function () {
-  this.age = 2037 - this.birthYear;
-  return this.age;
-},//function value(property of raju expression)
-
-//challenge
-//"Raju is a 23-year old teacher, and he has a driver's licnse"
-getSummary: function() {
-
- return  `${this.firstName} is a ${this.calcAge()} -year old ${this.birthYear.job}, and he has ${this.hasDriversLicense ? 'a' : 'not'} driver's license.`
+// Task 1: Create objects for mark Miller and john Smith
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
 }
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.68,
+}
+//Task 2: Create calcBMI Method for each object
+mark.calcBMI = function() {
+    this.bmi= this.mass / (this.height * this.height);
+    return this.bmi;
 };
-console.log( raju.calcAge(1991)); 
-console.log(raju.getSummary()); 
+john.calcBMI = function() {
+    this.bmi = this.mass / (this.height * this.height);
+    return  this.bmi;
+};
+//Task 3: compare BMIs and log the result
+const markBMI = mark.calcBMI()
+const JohnBMI = john.calcBMI()
+if(markBMI > JohnBMI){
+    console.log(
+        `${mark.fullName}'s BMI ${markBMI} is higher than ${john.fullName}'s ${JohnBMI})!`
+    );
+} else if (JohnBMI > markBMI) {
+    console.log(`${john.fullName}'s BMI ${JohnBMI} is higher than ${mark.fullName}'s (${markBMI})`
+    );
+} else{
+    console.log(`${mark.fullName} and ${john.fullName} have the same BMI (${markBMI})!`);    
+}
+
+    
